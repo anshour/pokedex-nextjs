c5b89f177187909d8ae75d08d9c009369166c21c
Anshor J <anshorje@gmail.com>
2025-07-30 14:19:47 +0700

    Add font-medium to Badge component styles

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -8,7 +8,7 @@ const Badge = ({ className, ...props }: BadgeProps) => {
   return (
     <div
       className={twMerge(
-        "inline-block bg-white/20 text-white px-2 sm:px-3 py-1 rounded-xl text-xs m-1",
+        "inline-block bg-white/20 text-white px-2 sm:px-3 py-1 rounded-xl text-xs m-1 font-medium",
         className
       )}
       {...props}
