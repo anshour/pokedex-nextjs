3bab3e773b30471d5f5ef091f5a855153990a261
Anshor J <anshorje@gmail.com>
2025-07-30 14:22:25 +0700

    Enhance query config with error handling and refetch options

--- src/config/query-config.ts
+++ src/config/query-config.ts
@@ -1,9 +1,16 @@
+import displayErrorToast from "@/utils/display-error-toast";
 import { QueryClientConfig } from "@tanstack/react-query";
 
 export const queryConfig: QueryClientConfig = {
   defaultOptions: {
     queries: {
       networkMode: "online",
+      refetchOnWindowFocus: false,
+      refetchOnMount: false,
+      throwOnError: (error: Error) => {
+        displayErrorToast(error);
+        return false;
+      },
       retry: 1,
     },
     mutations: {
