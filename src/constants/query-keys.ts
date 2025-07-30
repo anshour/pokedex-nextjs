b8ae3a9a32ecc6706e83c9e64cad7da8bd73d5a7
Anshor J <anshorje@gmail.com>
2025-07-30 14:22:55 +0700

    Add evolution chain and move query keys

--- src/constants/query-keys.ts
+++ src/constants/query-keys.ts
@@ -1,5 +1,12 @@
 export const QUERY_KEYS = {
-  POKEMON_LIST: (page: number, limit: number) => ["pokemon", "list", page, limit],
+  POKEMON_LIST: (page: number, limit: number) => [
+    "pokemon",
+    "list",
+    page,
+    limit,
+  ],
   POKEMON_DETAIL: (id: number) => ["pokemon", "detail", id],
   POKEMON_SPECIES: (id: number) => ["pokemon", "species", id],
+  POKEMON_EVOLUTION_CHAIN: (id: number) => ["pokemon", "evolution-chain", id],
+  POKEMON_MOVE: (id: number) => ["pokemon", "move", id],
 };
